0f3d8cb7f443db41135c906b116aacae50603c54
Prudence1021 <connorowenbrown@gmail.com>
2024-11-02 08:29:31 -0700

    ch.2:1 creation of TextNode class

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from textnode import *
+
+
+
+def main():
+
+
+    test_text = TextNode("this is a text node", TextType.ITALIC, "https://www.boot.dev")
+    print(test_text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
